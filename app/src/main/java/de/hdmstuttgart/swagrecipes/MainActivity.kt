4add299333495a65afdfa4213e684c2f11f5a1b3
lucas-goldner <lucas.goldner@googlemail.com>
2022-12-18 00:39:00 +0100

    feat: Implement splashscreen and app icons

--- app/src/main/java/de/hdmstuttgart/swagrecipes/MainActivity.kt
+++ app/src/main/java/de/hdmstuttgart/swagrecipes/MainActivity.kt
@@ -1,5 +1,6 @@
 package de.hdmstuttgart.swagrecipes
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,22 +11,30 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import de.hdmstuttgart.swagrecipes.databinding.ActivityMainBinding
+import androidx.core.splashscreen.SplashScreen
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private var contentHasLoaded = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-
+        // TODO: Load data before setup
+        contentHasLoaded = true
+        setupSplashScreen(splashScreen)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
-
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
@@ -34,6 +43,35 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAnchorView(R.id.fab)
                 .setAction("Action", null).show()
+        }
+    }
+
+    private fun setupSplashScreen(splashScreen: SplashScreen) {
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (contentHasLoaded) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else false
+                }
+            }
+        )
+
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val slideBack = ObjectAnimator.ofFloat(
+                splashScreenView.view,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.view.height.toFloat()
+            ).apply {
+                interpolator = DecelerateInterpolator()
+                duration = 500L
+                doOnEnd { splashScreenView.remove() }
+            }
+
+            slideBack.start()
         }
     }
 
