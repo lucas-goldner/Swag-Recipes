bebedd1a7da0b1e9530b7f152a7aec1f84d0e818
ak298 <ak298@hdm-stuttgart.de>
2023-02-07 06:43:29 +0100

    Finished AddRecipeActivity

--- app/src/main/java/de/hdmstuttgart/swagrecipes/ui/AddNewRecipeActivity.kt
+++ app/src/main/java/de/hdmstuttgart/swagrecipes/ui/AddNewRecipeActivity.kt
@@ -1,6 +1,8 @@
 package de.hdmstuttgart.swagrecipes.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,66 +12,186 @@ import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet.Constraint
+import androidx.recyclerview.widget.GridLayoutManager
 import de.hdmstuttgart.swagrecipes.R
+import de.hdmstuttgart.swagrecipes.data.model.ingredient.Ingredient
 import de.hdmstuttgart.swagrecipes.data.model.recipe.Recipe
 import de.hdmstuttgart.swagrecipes.databinding.ActivityAddNewRecipeBinding
+import de.hdmstuttgart.swagrecipes.ui.detail.RecipeDetailAdapter
+import java.util.*
+import kotlin.collections.ArrayList
 
-
+/**
+ * This class implements the AddNewRecipe Activity which allows the user to create his
+ * own Recipe by filling out all of the needed attributes to create a Recipe
+ */
 class AddNewRecipeActivity : AppCompatActivity() {
 
+    // This List stores all of the User recipes
+    lateinit var userRecipes: ArrayList<Recipe>
 
     private lateinit var binding: ActivityAddNewRecipeBinding
 
+    // Input Fields/Checkboxes
     private lateinit var titleInput: EditText
     private lateinit var instructionsInput: EditText
     private lateinit var readyInMinutesInput: EditText
+    private lateinit var servingsInput: EditText
     private lateinit var vegetarianCheckbox: CheckBox
     private lateinit var veganCheckbox: CheckBox
+    private lateinit var glutenFreeCheckbox: CheckBox
+    private lateinit var dairyFreeCheckbox: CheckBox
+    private lateinit var veryHealthyCheckbox: CheckBox
+    private lateinit var cheapCheckbox: CheckBox
+    private lateinit var ketogenicCheckbox: CheckBox
 
-
-    var inputCounter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         binding = ActivityAddNewRecipeBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_add_new_recipe)
+        setContentView(binding.root)
 
+        userRecipes = ArrayList<Recipe>()
+
+        // Binding Input Fields/Checkboxes
         titleInput = binding.titleInput
         instructionsInput = binding.instructionsInput
         readyInMinutesInput = binding.readyInMinutesInput
+        servingsInput = binding.servingsInput
         vegetarianCheckbox = binding.vegetarianCheckbox
         veganCheckbox = binding.veganCheckbox
+        glutenFreeCheckbox = binding.glutenFreeCheckbox
+        dairyFreeCheckbox = binding.dairyFreeCheckbox
+        veryHealthyCheckbox = binding.isHealthyInput
+        cheapCheckbox = binding.isCheapInput
+        ketogenicCheckbox = binding.ketogenic
 
+        // Adds more Input fields when button pressed
+        binding.addMoreIngredients.setOnClickListener {
+            addInputFields()
+        }
 
         binding.doneButton.setOnClickListener {
-            /*val recipe = Recipe(
+
+            // Create Recipe
+            val recipe = Recipe(
+                id = UUID.randomUUID().toString(),
                 title = titleInput.text.toString(),
+                ingredients = getInputs(),
+                imageURL = "NotImplementedYet",
                 instructions = instructionsInput.text.toString(),
                 readyInMinutes = readyInMinutesInput.text.toString().toInt(),
                 vegetarian = vegetarianCheckbox.isChecked,
-                vegan = veganCheckbox.isChecked
-            )
-             */
+                vegan = veganCheckbox.isChecked,
+                glutenFree = glutenFreeCheckbox.isChecked,
+                dairyFree = dairyFreeCheckbox.isChecked,
+                veryHealthy = veryHealthyCheckbox.isChecked,
+                veryPopular = false,
+                cheap = cheapCheckbox.isChecked,
+                ketogenic = ketogenicCheckbox.isChecked,
+                servings = servingsInput.text.toString().toInt()
+                )
+            // Add Recipe to List
+            userRecipes.add(recipe)
+
+            // Go back to BrowsePage
+            val intent = Intent(this, BrowseActivity::class.java)
+            startActivity(intent)
+
+            // Show all Recipes in Console
+            for ((index,recipe) in userRecipes.withIndex()){
+                Log.d("RecipeNumber", "Number: $index")
+                Log.d("RecipeName", recipe.title)
+                Log.d("RecipeInstructions", recipe.instructions)
+                Log.d("RecipeIngredients", recipe.ingredients.toString())
+
+            }
         }
-        //binding.addMoreIngredients.setOnClickListener { addInputFields() }
-
-
     }
 
+    /**
+     * This function creates a Linear Layout with 3 Input Fields inside of it. It represents
+     * a single Ingredient input of the user. This "package" of input fields can be generated
+     * as much as the user wants to
+     */
+    private fun addInputFields() {
 
+        // Linear Layout
+        val newInputFields = LinearLayout(this)
+        newInputFields.orientation = LinearLayout.HORIZONTAL
+        newInputFields.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
 
-    fun addInputFields(view: View) {
+        // Input field for Ingredient Name
+        val ingredientInput = EditText(this)
+        ingredientInput.id = View.generateViewId()
+        ingredientInput.layoutParams = LinearLayout.LayoutParams(
+            0,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            1f
+        ).apply {
+            setMargins(16.dpToPx(), 0, 16.dpToPx(), 0)
+        }
+        ingredientInput.hint = "Ingredient Name"
+        ingredientInput.inputType = InputType.TYPE_CLASS_TEXT
 
-        val inflater = LayoutInflater.from(this)
-        val newLinearLayout = inflater.inflate(R.layout.input_fields_layout, binding.ingredientsLinearLayout, false) as ConstraintLayout
-        newLinearLayout.id = inputCounter
-        binding.ingredientsLinearLayout.addView(newLinearLayout)
+        // Input field for Ingredient Amount
+        val ingredientAmountInput = EditText(this)
+        ingredientInput.id = View.generateViewId()
+        ingredientAmountInput.layoutParams = LinearLayout.LayoutParams(
+            0,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            0.5f
+        ).apply {
+            setMargins(16.dpToPx(), 0, 16.dpToPx(), 0)
+        }
+        ingredientAmountInput.hint = "Amount"
+        ingredientAmountInput.inputType = InputType.TYPE_CLASS_NUMBER
 
-        val ingredientInput = newLinearLayout.findViewById<EditText>(R.id.ingredientInput)
-        val ingredientAmountInput = newLinearLayout.findViewById<EditText>(R.id.ingredientAmountInput)
-        val ingredientUnitInput = newLinearLayout.findViewById<EditText>(R.id.ingredientUnitInput)
+        // Input field for Ingredient Unit
+        val ingredientUnitInput = EditText(this)
+        ingredientInput.id = View.generateViewId()
+        ingredientUnitInput.layoutParams = LinearLayout.LayoutParams(
+            0,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            0.5f
+        ).apply {
+            setMargins(16.dpToPx(), 0, 16.dpToPx(), 0)
+        }
+        ingredientUnitInput.hint = "Unit"
+        ingredientUnitInput.inputType = InputType.TYPE_CLASS_TEXT
 
-        inputCounter++
+        newInputFields.addView(ingredientInput)
+        newInputFields.addView(ingredientAmountInput)
+        newInputFields.addView(ingredientUnitInput)
+
+        // Add the whole Linear Layout with its 3 Input fields to the main layout
+        binding.ingredientsLinearLayout.addView(newInputFields)
     }
+
+    private fun Int.dpToPx(): Int {
+        return (this * resources.displayMetrics.density + 0.5f).toInt()
+    }
+
+    /**
+     * This function gets all of the inputs by the generated User input fields. It then
+     * creates a Ingredient based on the Inputs and stores them in a list returning them afterwards
+     */
+    private fun getInputs(): List<Ingredient> {
+        val ingredientList = mutableListOf<Ingredient>()
+
+        for (i in 0 until binding.ingredientsLinearLayout.childCount) {
+            val linearLayout = binding.ingredientsLinearLayout.getChildAt(i) as LinearLayout
+            val ingredientInput = linearLayout.getChildAt(0) as EditText
+            val ingredientAmountInput = linearLayout.getChildAt(1) as EditText
+            val ingredientUnitInput = linearLayout.getChildAt(2) as EditText
+
+            var ingredient = Ingredient(ingredientInput.text.toString(),ingredientAmountInput.text.toString().toDouble(),ingredientUnitInput.text.toString())
+            ingredientList.add(ingredient)
+        }
+        return ingredientList
+    }
+
 }
